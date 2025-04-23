de4be8559e50d081fec64ef1c86671cbf6aef793
André Branco <andre_c_branco@estudante.sc.senai.br>
2025-04-23 15:44:52 -0300

    Remover arquivos indesejados

--- Downloads/prova/prova/src/main/java/com/basico/prova/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.basico.prova.repository;
-
-import com.basico.prova.entities.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
