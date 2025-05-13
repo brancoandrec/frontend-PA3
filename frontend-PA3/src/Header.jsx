1933ce2e250b516c5cb3aea95e9a0b46be7ba556
André Branco <andre_c_branco@estudante.sc.senai.br>
2025-05-13 13:25:59 -0300

    header

--- frontend-PA3/src/Header.jsx
+++ frontend-PA3/src/Header.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import logoisi from './assets/logo.png'; 
+import logosesi from './assets/logo-sesi.webp';
 import logosenai from './assets/logo-senai.webp';
+import iconuser from './assets/icon-user.png';
+import iconexit from './assets/icon-exit.png';
 import './styles.css';
 
 const Header = () => {
   return (
-    <header className="site-header">
-        <div className='header-left'>
-            <img className="logo-senai-menu" src={logosenai} alt="Logo Senai" />
-        </div>
-        <div className='header-center'>
-            <img className="logo-isi-menu" src={logoisi} alt="Logo Instituto Senai de Inovação" />
-        </div>
-        <div className='header-right'>
+    <header className="site-header flex justify-between items-center w-full px-6">
+  <div className="header-left flex flex-col items-center space-y-2">
+    <img className="logo-senai-menu" src={logosesi} alt="Logo Sesi" />
+    <img className="logo-senai-menu" src={logosenai} alt="Logo Senai" />
+  </div>
+  <div className="header-center flex justify-center flex-grow">
+    <img className="logo-isi-menu" src={logoisi} alt="Logo Instituto Senai de Inovação" />
+  </div>
+  <div className="header-right flex items-center space-x-3">
+    <div className="text-sm">
+      <p className="text-white font-medium">Bem-vindo, Usuário</p>
+      <div className="flex items-center space-x-1 justify-end">
+        <button className="text-blue-500 hover:underline">Sair</button>
+        <img
+          src={iconexit}
+          alt="Sair"
+          className="w-4 h-4"
+        />
+      </div>
+    </div>
+    <img
+      src={iconuser}
+      alt="Avatar"
+      className="w-10 h-10 rounded-full"
+    />
+  </div>
+</header>
 
-        </div>
-    </header>
+
   );
 };
 
---
+++ frontend-PA3/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Certifique-se de incluir o index.html
+    "./src/**/*.{js,jsx,ts,tsx}" // Incluir todos os arquivos dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
